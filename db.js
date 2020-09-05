00e97a9f97e5d906de57b4ea9e2241a87078194a
SeojinSeojin <1106laura@naver.com>
2020-09-06 01:28:50 +0900

    진짜 마지막..

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ import ArchieveModel from "./db/archieve.js";
 const db = () => {
     function connect() {
         mongoose.connect(
-            "mongodb://heroku_7qq017s1:qn7o7ubt2biskt9nv0kn1dicf6@ds253567.mlab.com:53567/heroku_7qq017s1/", { useUnifiedTopology: true, useNewUrlParser: true },
+            "mongodb://heroku_7qq017s1:qn7o7ubt2biskt9nv0kn1dicf6@ds253567.mlab.com:53567/heroku_7qq017s1 ", { useUnifiedTopology: true, useNewUrlParser: true },
             function(err) {
                 if (err) {
                     console.error("mongodb connection error\n", err);
